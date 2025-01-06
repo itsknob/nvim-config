e2b1640bf3d5907469721f5b86ffba0924c9b4e0
Stephen Reilly <stephen.reilly@cimpress.io>
2025-01-06 13:03:50 -0500

    feat: update which key mappings and ensure transparent background

--- lua/knob/lazy.lua
+++ lua/knob/lazy.lua
@@ -4,15 +4,17 @@ return {
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
-
+  {
+      "jasonpanosso/harpoon-tabline.nvim",
+      dependencies = { "ThePrimeagen/harpoon" }
+  },
   'tpope/vim-fugitive', -- git inside vim
   'jiangmiao/auto-pairs', -- brackets are cool
-
   'preservim/nerdtree',
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} }, -- Comment keybindings
-
+  -- { 'numToStr/Comment.nvim', opts = {} }, -- Comment keybindings
+  --
   { -- which-key
     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
@@ -21,19 +23,26 @@ return {
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { "<leader>c", group = "[C]ode" },
+        { "<leader>c_", hidden = true },
+        { "<leader>d", group = "[D]ocument" },
+        { "<leader>d_", hidden = true },
+        { "<leader>h", group = "Git [H]unk" },
+        { "<leader>h_", hidden = true },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>r_", hidden = true },
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>s_", hidden = true },
+        { "<leader>t", group = "[T]oggle" },
+        { "<leader>t_", hidden = true },
+        { "<leader>w", group = "[W]orkspace" },
+        { "<leader>w_", hidden = true },
       }
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      require('which-key').add({
+    { "<leader>h", desc = "Git [H]unk", mode = "v" },
+  }, { mode = 'v' })
     end,
   },
 
@@ -57,6 +66,13 @@ return {
     init = function()
       vim.cmd.colorscheme 'catppuccin-macchiato'
       vim.cmd.hi 'Comment gui=none'
+      -- transparent background
+      vim.cmd [[
+          highlight Normal guibg=NONE
+          highlight NonText guibg=NONE
+          highlight Normal ctermbg=NONE
+          highlight NonText ctermbg=NONE
+      ]]
     end,
   },
   { -- todo-comments
@@ -87,10 +103,11 @@ return {
   -- { import = 'custom.plugins.nvim-jdtls' },
   { import = 'custom.plugins.nvim-lspconfig' },
   { import = 'custom.plugins.telescope' },
-  { import = 'custom.plugins.treesitter' },
+  -- { import = 'custom.plugins.treesitter' },
   { import = 'custom.plugins.vim-test' },
   { import = 'custom.plugins.tmux' },
   { import = 'custom.plugins.conform' },
+  { import = 'custom.plugins.completion' },
   -- { import = 'custom.plugins.neotest.bak' },
 
   -- [[
