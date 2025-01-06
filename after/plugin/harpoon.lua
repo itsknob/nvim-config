b20bf1d4a7b9005d00418c23b85db26cdd451e79
Stephen Reilly <stephen.reilly@cimpress.io>
2025-01-06 13:04:37 -0500

    fix: update harpoon plugin settings to harpoon2 syntax

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,29 +1,23 @@
-local m = require 'harpoon.mark'
-local ui = require 'harpoon.ui'
+local harpoon = require 'harpoon'
+local harpoontabline = require 'harpoon-tabline'
+local m = harpoon.mark
+local ui = harpoon.ui
+
+-- REQUIRED --
+harpoon:setup()
+harpoontabline.setup()
+-- REQUIRED --
 
 function addAndRedraw()
   m.add_file()
   vim.cmd 'redrawt'
 end
 
-vim.keymap.set('n', '<leader>a', m.add_file, { desc = '[A]dd file to harpoon' })
-vim.keymap.set('n', '<leader>h', ui.toggle_quick_menu, { desc = '[A]dd file to harpoon' })
-vim.keymap.set('n', '<M-j>', ui.nav_prev, { desc = '[P]revious harpoon file' })
-vim.keymap.set('n', '<M-k>', ui.nav_next, { desc = '[N]ext harpoon file' })
 
-local tmux = require 'harpoon.tmux'
-vim.keymap.set('n', '<leader>x', function()
-  tmux.gotoTerminal(1)
-end, { desc = 'Go to Termainl' })
-
-require('harpoon').setup {
-  menu = {
-    width = math.floor(vim.api.nvim_win_get_width(0) * 0.6),
-  },
-  tabline = true,
-  tabline_prefix = ' ',
-  tabline_suffix = ' ',
-}
+vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = '[A]dd file to harpoon' })
+vim.keymap.set('n', '<leader>h', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = '[A]dd file to harpoon' })
+vim.keymap.set('n', '<M-j>', function() harpoon:list():prev() end, { desc = '[P]revious harpoon file' })
+vim.keymap.set('n', '<M-k>', function() harpoon:list():next() end, { desc = '[N]ext harpoon file' })
 
 -- Fancy Tabline Formatting showing Active Harpoon
 vim.cmd 'highlight! HarpoonInactive guibg=NONE guifg=#63698c'
@@ -31,3 +25,10 @@ vim.cmd 'highlight! HarpoonActive guibg=NONE guifg=white'
 vim.cmd 'highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7'
 vim.cmd 'highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7'
 vim.cmd 'highlight! TabLineFill guibg=NONE guifg=white'
+
+-- TODO - move to this 
+vim.api.nvim_set_hl(0, 'HarpoonActive', { foreground = 'white', background = 'NONE' })
+vim.api.nvim_set_hl(0, 'HarpoonInactive', { foreground = '#63698c', background = 'NONE' })
+vim.api.nvim_set_hl(0, 'HarpoonNumberActive', { foreground = '#7aa2f7', background = 'NONE' })
+vim.api.nvim_set_hl(0, 'HarpoonNumberInactive', { foreground = '#7aa2f7', background = 'NONE' })
+vim.api.nvim_set_hl(0, 'TabLineFill', { foreground = 'white', background = 'NONE' })
