9025e27e668481b2f5fd03e242e2c09fcc09be12
itsknob <itsknob@gmail.com>
2025-04-27 10:12:45 -0400

    feat: update next diagnostic, add eZFold

--- lua/defaults/keymap.lua
+++ lua/defaults/keymap.lua
@@ -5,7 +5,7 @@ vim.keymap.set('n', '<M-n>', ':cnext', { desc = '[N]ext item in quickfix list' }
 vim.keymap.set('n', '<M-p>', ':cprev', { desc = '[P]revious item in quickfix list' })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_next, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
@@ -20,7 +20,7 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = "Don't go to end of first line 
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = "Don't go to end of first line when Joining" })
 vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Center Screen when Searching' })
 vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Center Screen when Searching Reverse' })
-vim.keymap.set('n', 'zF', '$zf%', { desc = 'Create [F]old' })
+vim.keymap.set('n', 'zF', '$zf%', { desc = "Fold from end of line to matching bracket" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
