c29ddba1bd600db35cb1e5a1673e67d00ac6c33a
Stephen Reilly <stephen.reilly@cimpress.io>
2025-05-08 15:18:16 -0400

    feat: add eslint to lspconfig

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -167,39 +167,40 @@ return { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-        -- clangd = {},
-        gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {
-            name = "typescript-language-server"
-        },
-        --
-        -- jdtls = {},
+      -- clangd = {},
+      gopls = {},
+      -- pyright = {},
+      -- rust_analyzer = {},
+      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+      --
+      -- Some languages (like typescript) have entire language plugins that can be useful:
+      --    https://github.com/pmizio/typescript-tools.nvim
+      --
+      -- But for many setups, the LSP (`tsserver`) will work just fine
+      tsserver = {
+        name = 'typescript-language-server',
+      },
+      --
+      -- jdtls = {},
 
-        lua_ls = {
-            -- cmd = {...},
-            -- filetypes = { ...},
-            -- capabilities = {},
-            settings = {
-                Lua = {
-                    completion = {
-                        callSnippet = 'Replace',
-                    },
-                    -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                    diagnostics = { disable = { 'missing-fields' } },
-                },
+      lua_ls = {
+        -- cmd = {...},
+        -- filetypes = { ...},
+        -- capabilities = {},
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = 'Replace',
             },
+            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            diagnostics = { disable = { 'missing-fields' } },
+          },
         },
-        jsonls = {
-            capabilities = capabilities,
-        },
+      },
+      jsonls = {
+        capabilities = capabilities,
+      },
+      eslint = {},
     }
 
     -- Ensure the servers and tools above are installed
@@ -208,7 +209,7 @@ return { -- LSP Configuration & Plugins
     --    :Mason
     --
     --  You can press `g?` for help in this menu.
-    
+
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
@@ -216,6 +217,7 @@ return { -- LSP Configuration & Plugins
       'jsonls',
       'stylua', -- Used to format Lua code
       'tsserver',
+      'eslint-lsp',
     })
     -- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -223,17 +225,17 @@ return { -- LSP Configuration & Plugins
     require('mason-lspconfig').setup {
       handlers = {
         function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            -- server.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+          local server = servers[server_name] or {}
+          -- This handles overriding only values explicitly passed
+          -- by the server configuration above. Useful when disabling
+          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          -- server.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
 
-            require('lspconfig')[server_name].setup(server)
+          require('lspconfig')[server_name].setup(server)
         end,
         jdtls = function()
-            print 'noop'
+          print 'noop'
         end,
       },
     }
